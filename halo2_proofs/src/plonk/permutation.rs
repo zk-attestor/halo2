fac6fb5197bcefe9641c31e8f250534411524da3
Nalin Bhardwaj <nalinbhardwaj@nibnalin.me>
2022-10-07 11:26:33 -0700

    VerifyingKey Serialization: merge Nalin's PR https://github.com/zcash/halo2/pull/661 that allows for vkey serialization/deserialization and fixes the previous selector optimization issue

--- halo2_proofs/src/plonk/permutation.rs
+++ halo2_proofs/src/plonk/permutation.rs
@@ -83,6 +83,21 @@ impl<C: CurveAffine> VerifyingKey<C> {
     pub fn commitments(&self) -> &Vec<C> {
         &self.commitments
     }
+
+    pub(crate) fn write<W: io::Write>(&self, writer: &mut W) -> io::Result<()> {
+        for commitment in &self.commitments {
+            writer.write_all(commitment.to_bytes().as_ref())?;
+        }
+
+        Ok(())
+    }
+
+    pub(crate) fn read<R: io::Read>(reader: &mut R, argument: &Argument) -> io::Result<Self> {
+        let commitments = (0..argument.columns.len())
+            .map(|_| C::read(reader))
+            .collect::<Result<Vec<_>, _>>()?;
+        Ok(VerifyingKey { commitments })
+    }
 }
 
 /// The proving key for a single permutation argument.
